6bd0dfe9e2c907291b1c38a459dba7686df4b20d
Brian Christiansen <brian@schedmd.com>
2020-01-08 10:04:44 -0700

    Stuff

--- src/slurmctld/job_mgr.c
+++ src/slurmctld/job_mgr.c
@@ -13746,9 +13746,14 @@ static int _update_job(job_record_t *job_ptr, job_desc_msg_t *job_specs,
 
 	if (job_specs->mail_user) {
 		xfree(job_ptr->mail_user);
-		job_ptr->mail_user = xstrdup(job_specs->mail_user);
-		sched_info("%s: setting mail_user to %s for %pJ",
-			   __func__, job_ptr->mail_user, job_ptr);
+		if (!strlen(job_specs->mail_user)) {
+			sched_info("%s: setting mail_user to job's user for %pJ",
+				   __func__, job_ptr);
+		} else {
+			job_ptr->mail_user = xstrdup(job_specs->mail_user);
+			sched_info("%s: setting mail_user to %s for %pJ",
+				   __func__, job_ptr->mail_user, job_ptr);
+		}
 	}
 
 	/*
